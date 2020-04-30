2b86f2ee54f322410762c1195818c3afccaf3a21
vegadevh <00148816@uca.edu.sv>
2020-04-30 16:21:19 -0600

    Controlador para ResponseBody con objetos

--- capas/src/main/java/com/uca/capas/controller/MainController.java
+++ capas/src/main/java/com/uca/capas/controller/MainController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.uca.capas.domain.Student;
 
@@ -28,6 +30,22 @@ public class MainController {
 				new Student("nombre1", "Apellido","10/10/10","Ing en limones", true),
 				new Student("nombre1", "Apellido","10/10/10","Shampoo", true));
 				
+	}
+	
+	@GetMapping("/index")
+	public String inicio(Student student) {
+		return "index";
+	}
+	
+	@PostMapping("/formData")
+	public ModelAndView procesar(Student student) {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("prueba");
+		mav.addObject("Estado", student.delegateEstado());
+		mav.addObject("student",student);
+		
+		return mav;
 	}
 	
 }
